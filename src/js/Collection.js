f31183fc275212b76bcd5079e80b4f4af79f4eb6
beatniked <101743060+beatniked@users.noreply.github.com>
2022-05-02 10:15:16 +0200

    Add files via upload

--- src/js/Collection.js
+++ src/js/Collection.js
@@ -1,1 +1,29 @@
+let slideIndex = [1, 1];
+let slideId = ["mySlides1", "mySlides2"]
+let dotId = ["dot", "dot2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  let dots = document.getElementsByClassName(dotId[no]);
+  if (n > x.length) { slideIndex[no] = 1 }
+  if (n < 1) { slideIndex[no] = x.length }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex[no] - 1].style.display = "block";
+  dots[slideIndex[no] - 1].className += " active";
+}
